57947e980c1a593bb9e9c938fc7c078b2812aafd
Lautaro Montaña <joselautarom@gmail.com>
2023-10-31 18:03:06 -0300

    Clase 1,2,3

---
+++ src/Clase4/Clase4.java
@@ -0,0 +1,56 @@
+package Clase4;
+//Palabra package: Nos indica que esta java class proviene de Clase4
+
+public class Clase4 {
+  /*
+ public: es una palabra reservada que determina un modificador de acceso, el cual nos va a permitir 
+ hacer que una clase es de acceso a esta clase de java desde cualquier punto de nuestra app
+  class: es una palabra reservada, que determina la estructura básica de una clase
+ */
+      /*
+        void: es una palabra reservada que nos indica que no nos retorna nada esta funcion
+        main: nos indica que es el método principal y que nos permite la ejecución de nuestra app
+        syste.out.println: nos permite a nosotros devs poder imprimir resultados en la consola
+        de manera rápida
+        */
+    
+    public static void main(String[] args) {
+        System.out.println("Clase 4");
+        
+        
+        
+        /*
+        Tipos de datos primitivos: 8
+        
+        Numeros enteros
+        
+        int 4 bytes de espacio de almacenamiento en memoria y 
+        va desde -2.147.483.648 hasta 2.147.483.647
+        
+        short: 2 bytes de espacio en memoria y van desde
+        -32.768 hasta 32.267
+        
+        long: 8 bytes de espacio de almacenamiento en memoria subfijo L 50.000.000.000.000
+        
+        byte: 1 byte de espaciod de almacenamiento en memoria 
+        van desde -128 hasta 127
+        
+        Numeros decimales:
+        
+        float: 4 bytes de espacio de almacenamiento en memoria van desde las 6 o 7 cifras decimales
+        5,798852 subfijo F
+        
+        double: 8 bytes de espacio de almacenamiento y van desde 15 cifras decimales
+        5,484544523989674
+        
+        char: representa caracteres de la 'a' a la 'z'
+        
+        boolean: true o false
+        
+        */
+        
+        
+    
+    }   
+
+}
