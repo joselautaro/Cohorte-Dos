a532883a5ab3a7d84ece4aae226be72cb09b0392
Lautaro Montaña <joselautarom@gmail.com>
2023-11-10 19:07:40 -0300

    Clase 9

---
+++ src/Clase9/Auto.java
@@ -0,0 +1,34 @@
+package Clase9;
+
+
+public class Auto {
+    
+    //Atributos == Características
+    
+    //Métodos == funciones
+    
+    
+    //Declaramos los atributos de nuestro objeto
+    String color;
+    
+    String marca;
+    
+    String modelo;
+    
+    //Esta es una instancia de nuestra clase auto con sus atributos dados
+    public Auto( String color, String marca, String modelo){
+        this.color = color;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    //Asignamos métodos == funciones
+    public void encender(){
+        System.out.println("El auto " + marca + " está encendiendo");
+    }
+    public void apagar(){
+        System.out.println("El auto " + marca + " está apagado");
+    }
+    //Creación de un objeto auto
+}
+
